d3bbd84c7e254f6f427d856d1f25b22eb6a3b4b0
Ophilia <ophiliamoholola@gmail.com>
2023-05-16 11:23:30 -0700

    TASK 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - prints its name
  * @argc: argument count
  * @argv: argument vector
- * Return: Alwaya vector
+ * Return: Always zero
  */
 
 int main(int argc, char *argv[])
@@ -13,5 +13,5 @@ int main(int argc, char *argv[])
 	(void) argc;
 	printf("%s\n", argv[0]);
 
-	return(0);
+	return (0);
 }
